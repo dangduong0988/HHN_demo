720895c750b8e08e551a29cb970162e77d7ab237
dangduong0127 <118369511+dangduong0988@users.noreply.github.com>
2023-10-05 16:36:36 +0700

    release

---
+++ login.js
@@ -0,0 +1,13 @@
+// alert("")
+const btn = document.getElementById("Send");
+btn.addEventListener("click", function () {
+  const eml = document.getElementById("Email");
+  const mk = document.getElementById("mk");
+  if (eml.value == "") {
+    alert("Bạn chưa nhập email");
+  } else if (mk.value == "") {
+    alert("Bạn chưa nhập mật khẩu");
+  }
+});
+
+w3.slideshow(".slides", 2000);
